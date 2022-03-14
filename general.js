ed46aca8438502bd552e7ea92a6174961bfdd745
Frank Emanuel <emanuef@algonquincollege.com>
2022-03-14 12:44:11 -0400

    Working Files for lecture

---
+++ general.js
@@ -0,0 +1,12 @@
+function closeLogin() {
+
+    document.getElementById("loginBox").style.display = "none";
+
+}
+
+function loginBox() {
+
+    document.getElementById("loginBox").style.display = "block";
+
+
+}
